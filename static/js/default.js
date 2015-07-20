f9520392d489f3f3d5156c9997186a06a148754c
Edward Chong <edward@ed-mart.com>
2015-07-20 15:29:57 -0700

    modified fat tabs widget to not load all video's at once.

--- static/js/default.js
+++ static/js/default.js
@@ -59,7 +59,6 @@ $(document).ready(function () {
 
 	if($('form').filter('.has-slide').length) {
 		$.getScript('/static/js/events.min.js');
-		//eventForm();
 	}
 
 //Dotdotdot
@@ -167,6 +166,9 @@ $(document).ready(function () {
 		});
 	}
 
+	//Tabs
+	fatTabsResize();
+
 //Ooyala player popup
 	$('body').on('click', '.ooplaylist', function (e) {
 		e.preventDefault();
@@ -222,69 +224,90 @@ $(document).ready(function () {
 	grayscaleImage();
 });
 
+addResize('fatTabsResize');
 addResize('offCanvasResize');
 addResize('resizeFourColumnFilmstripCarousel');
 addResize('toggleResize');
 addResize('grayscaleImage');
 
-function resizeFourColumnFilmstripCarousel() {
-	(function() {
-		if ($('.screenshot-carousel').length && !$('html').hasClass('home')) {
-			if($.fn.slidePagination2) {
-				init();
-			}
-			else {
-				$.getScript('/static/library/jQuery/jquery.slidepagination.min.js', function() {
-					init();
-				});
-			}
+function fatTabsResize() {
+	if (pageWidth < 768) {//mobile
+		try {
+			$('.fat-tabs').tabs('destroy');
 		}
+		catch(e) {
 
-		function init() {
-			if ($('.screenshot-carousel').length) {
-				var sliderOptions = {
-					list: '> ul',
-					column: 4,
-					row: 1,
-					largeArrow: true,
-					pagination: [
+		}
+	}
+	else {
+		$('.fat-tabs').tabs({
+			activate: function(event, ui) {
+				ui.newPanel.trigger('tab.visible');
+				ui.oldPanel.trigger('tab.hidden');
+			}
+		});
+	}
+}
+
+function resizeFourColumnFilmstripCarousel(parentSelector) {
+	if(typeof parentSelector == 'undefined') {
+		parentSelector = 'body';
+	}
+
+	if ($(parentSelector).find('.screenshot-carousel').length && !$('html').hasClass('home')) {
+		if ($.fn.slidePagination2) {
+			init();
+		}
+		else {
+			$.getScript('/static/library/jQuery/jquery.slidepagination.min.js', function () {
+				init();
+			});
+		}
+	}
+
+	function init() {
+		if ($(parentSelector).find('.screenshot-carousel').length) {
+			var sliderOptions = {
+				list: '> ul',
+				column: 4,
+				row: 1,
+				largeArrow: true,
+				pagination: [
+					{type: 'prepend', nextArrow: false, align: 'left'},
+					{type: 'prepend', prevArrow: false}
+				]
+			};
+
+			$(parentSelector).find('.screenshot-carousel').each(function () {
+				var options = sliderOptions;
+
+				if ($(this).data('large-arrows') == 'true') {
+					options.largeArrow = true;
+					options.pagination = [
 						{type: 'prepend', nextArrow: false, align: 'left'},
 						{type: 'prepend', prevArrow: false}
-					]
-				};
+					];
+				}
 
-				$('.screenshot-carousel').each(function () {
-					var options = sliderOptions;
+				if ($(this).data('row')) {
+					options.row = $(this).data('row');
+				}
 
-					if ($(this).data('large-arrows') == 'true') {
-						options.largeArrow = true;
-						options.pagination = [
-							{type: 'prepend', nextArrow: false, align: 'left'},
-							{type: 'prepend', prevArrow: false}
-						];
-					}
+				if (pageWidth >= 992) {
+					options.column = $(this).data('col') ? $(this).data('col') : 4;
+				}
+				else if (pageWidth >= 768) { //tablet
+					options.column = $(this).data('col-sm') ? $(this).data('col-sm') : 3;
+				}
+				else if (pageWidth < 768) { //mobile
+					options.column = $(this).data('col-xs') ? $(this).data('col-xs') : 1;
+				}
 
-					if($(this).data('row')) {
-						options.row = $(this).data('row');
-					}
-
-					if (pageWidth >= 992) {
-						options.column = $(this).data('col') ? $(this).data('col') : 4;
-					}
-					else if (pageWidth >= 768) { //tablet
-						options.column = $(this).data('col-sm') ? $(this).data('col-sm') : 3;
-					}
-					else if (pageWidth < 768) { //mobile
-						options.column = $(this).data('col-xs') ? $(this).data('col-xs') : 1;
-					}
-
-					$(this).slidePagination2('destroy');
-					$(this).slidePagination2(options);
-				});
-			}
+				$(this).slidePagination2('destroy');
+				$(this).slidePagination2(options);
+			});
 		}
-
-	})();
+	}
 }
 
 function offCanvasResize() {
@@ -448,6 +471,10 @@ function offCanvas() {
 			$('#off-canvas').css('left', 0);
 
 			$(target).trigger('offcanvas-show');
+
+			setTimeout(function() {
+				resizeFourColumnFilmstripCarousel('#off-canvas');
+			}, 100);
 		});
 	});
 }
@@ -512,111 +539,3 @@ function grayscaleImage() {
 		}
 	}
 }
-
-/*function eventForm() {
-	$('.has-slide').each(function() {
-		var wrapper = $(this).find('> div'), len = wrapper.children().length;
-
-		if($('html').hasClass('ie9')) {
-			$(this).css({width: $(this).width()});
-			wrapper.css('width', $(this).width());
-			wrapper.children().css('width', $(this).width()).filter(':gt(0)').hide();
-		}
-		else {
-			wrapper.css({width: len * 100 + '%'}).find('> div').css('width', 100/len + '%');
-		}
-
-		if(!wrapper.find('> div:eq(0)').is(':visible')) {
-			//TODO: Need to find a way to detect if form is in view.
-		}
-		else {
-			//Set height to the wrapper to prevent extra white space below if other pages are not the same height;
-			//wrapper.css({minHeight: wrapper.find('> div:eq(0)').outerHeight(true)});
-		}
-
-		//Only make first page visible.
-		wrapper.find('> div:gt(0)').hide();
-
-		//Bind event
-		$(this).on('click', '.goto', function(e) {
-			e.preventDefault();
-
-			var newPage = $(this).data('page'),
-					slidePages = $(this).parents('.slide-page').parent().find('.slide-page'),
-					curPage = slidePages.index($(this).parents('.slide-page'));
-
-			if($('html').hasClass('ie9')) {
-				wrapper.children().hide().stop().filter(':eq(' + newPage + ')').show();
-			}
-			else {
-				//Show all pages
-				slidePages.show();
-				wrapper.css('left', -1 * (curPage * 100) + '%');
-
-				wrapper.animate({
-					left: -1 * (newPage * 100) + '%'
-				}, 500, function() {
-					slidePages.hide().stop().filter(':eq(' + newPage + ')').show();
-					wrapper.css('left', 0);
-				});
-			}
-		});
-
-		$(this).css('visibility', 'visible');
-	});
-
-	$('#event-container')
-			.on('click', 'tbody > tr', function(e) {
-				if(!(e.target.nodeName == 'INPUT' && e.target.type == 'checkbox')) {
-					$(this).find(':input[type=checkbox]').trigger('click');
-				}
-			})
-			.on('change', ':input[type=checkbox]', function (e) {
-				var checked = 0, map = ['', 'date', 'time', 'location', 'type', 'duration'], classMap = {
-					'online': 'event-online',
-					'on-demand': 'event-on-demand',
-					'in-person': 'event-in-person'
-				}, months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-				//Clear out display detail
-				$('.event-detail-container').empty();
-
-				$('#event-container').find(':input[type=checkbox]').each(function () {
-					if ($(this).is(':checked')) {
-						checked++;
-
-						var eventType = $(this).parents('tr').data('event-type'),
-								code = (eventType == 'on-demand') ? $('#date-widget-on-demand-tpl').html():$('#date-widget-tpl').html(),
-								date = null;
-
-						$(this).parents('tr').find('> td').each(function(indx, elem) {
-							if(map[indx] != '') {
-								code = code.replace(new RegExp('\\[\\[' + map[indx] + '\\]\\]', 'g'), elem.innerHTML);
-							}
-
-							if(map[indx] == 'date') {
-								date = new Date(elem.innerHTML.replace('.', ''));
-							}
-
-							if(map[indx] == 'type') {
-								type = elem.innerHTML;
-							}
-						});
-
-						//Apply date
-						code = code.replace('[[month]]', months[date.getMonth()]);
-						code = code.replace('[[day]]', date.getDate());
-
-						//Apply date widget header color
-						code = code.replace('[[headerclass]]', classMap[eventType]);
-
-						$('.event-detail-container').append(code);
-					}
-				});
-
-
-
-				$('#event-next-button').prop('disabled', checked ? false:true);
-			});
-}*/
-
--- widgets/fat-tabs/index.js
+++ widgets/fat-tabs/index.js
@@ -1,7 +1,4 @@
 $(function () {
-  //trigger tabs
-  $('.fat-tabs').tabs();
-
   createPlayers();
   resizeAwareness();
 });
@@ -9,17 +6,7 @@ $(function () {
 addResize('resizeAwareness');
 
 function resizeAwareness() {
-
-  if (pageWidth < 768) {//mobile
-    try {
-      $('.fat-tabs').tabs('destroy');
-    }
-    catch(e) {
-
-    }
-
-  }
-  else {
+  if (pageWidth >= 768) {
     //off canvas mode exit, reverts all the changes back
     if($('#off-canvas').is(':visible')) {
       $('.site-wrapper').show();
@@ -30,24 +17,50 @@ function resizeAwareness() {
       //append off canvas target back again
       $($('#off-canvas').data('target')).html($('#off-canvas').find('.off-canvas-content').children());
     }
-
-    $('.fat-tabs').tabs();
   }
 }
 
 function createPlayers() {
-  OO.ready(function () {
-    var players = ['NS-player','IAM-player','DP-player','IM-player','WSM-player','EM-player','PM-player'],
-        videoIds = ['lxbnlyazqgUTIEW_AQ1nIauBYIXwkHAr','w5MzA4dDpIw3uJL24fS8LaS3COFbYq-Z','R2cjA2dDpLyvaZ29i3XwVTsdqpfUX6MA',
-          'R0d3lyazpKNe0EN0f5dZlkgtBQhAUJZx','15Z2Fjcjo4neK1tNi60T490aBCPARlZh','AxNW54cjpyQY8Xg_DuI3-6ZegHP15DwC',
-          'AxNW54cjpyQY8Xg_DuI3-6ZegHP15DwC']
+	$.getScript('/static/library/jQuery/jquery.watch.min.js', function() {
+		OO.ready(function () {
+			var players = ['NS-player','IAM-player','DP-player','IM-player','WSM-player','EM-player','PM-player'],
+				videoIds = ['lxbnlyazqgUTIEW_AQ1nIauBYIXwkHAr','w5MzA4dDpIw3uJL24fS8LaS3COFbYq-Z','R2cjA2dDpLyvaZ29i3XwVTsdqpfUX6MA',
+					'R0d3lyazpKNe0EN0f5dZlkgtBQhAUJZx','15Z2Fjcjo4neK1tNi60T490aBCPARlZh','AxNW54cjpyQY8Xg_DuI3-6ZegHP15DwC',
+					'AxNW54cjpyQY8Xg_DuI3-6ZegHP15DwC']
 
-    $.each(players, function (index, value) {
-      var value = OO.Player.create( value ,videoIds[index] , {
-        onCreate: OOCreate,
-        autoplay: false,
-        wmode: 'transparent'
-      });
-    });
-  });
+			$.each(players, function (index, value) {
+				if($('#' + value).is(':visible')) {
+					OO.Player.create( value ,videoIds[index] , {
+						onCreate: OOCreate,
+						autoplay: false,
+						wmode: 'transparent'
+					});
+				}
+				else {
+					onVisibleLoadVideo(value, videoIds[index]);
+				}
+			});
+		});
+	});
+}
+
+function onVisibleLoadVideo(id, videoID) {
+	var elem = prevElem = $('#' + id);
+
+	while(!elem.is(':visible')) {
+		prevElem = elem;
+		elem = elem.parent();
+	}
+
+	prevElem.watch('display', function() {
+		if($(this).is(':visible') && !$('#' + id).data('loaded')) {
+			OO.Player.create(id ,videoID , {
+				onCreate: OOCreate,
+				autoplay: false,
+				wmode: 'transparent'
+			});
+
+			$('#' + id).data('loaded', true);
+		}
+	});
 }
