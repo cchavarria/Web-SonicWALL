e86b87e320e933cd429c3e07931118d58f79fa87
Edward Chong <edward@ed-mart.com>
2015-07-28 13:43:00 -0700

    ability to download the processed email.

--- email/index.php
+++ email/index.php
@@ -1,1 +1,16 @@
-<?php require 'widget.class.php'; require 'Emogrifier.php'; $widget = new Widget(); echo $widget->content(); ?>
+<?php
+    require 'widget.class.php';
+    require 'Emogrifier.php';
+
+    $widget = new Widget();
+
+    if(isset($_GET['download'])) {
+        $info = pathinfo($_GET['page']);
+
+        header("Content-Type: text/html");
+        header("Content-Transfer-Encoding: Ascii");
+        header("Content-disposition: attachment; filename=\"{$info['basename']}\"");
+    }
+
+    echo $widget->content();
+?>
