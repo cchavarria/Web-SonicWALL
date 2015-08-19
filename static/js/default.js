3a072e5a83c01f4c9bb6876161bdecd5634c2f65
Edward Chong <edward@ed-mart.com>
2015-08-19 11:17:41 -0700

    dynamically load video-player.min.js

--- static/js/default.js
+++ static/js/default.js
@@ -430,16 +430,18 @@ function loadOoyala(parentSelector) {
 			init();
 		}
 		else {
-			if ($('html').hasClass('ie9') || $('html').hasClass('ie8')) {
-				$.getScript('//player.ooyala.com/v3/9eba220ad98c47cda9fdf6ba82ce607a', function () {
-					init();
-				});
-			}
-			else {
-				$.getScript('//player.ooyala.com/v3/9eba220ad98c47cda9fdf6ba82ce607a?platform=html5', function () {
-					init();
-				});
-			}
+			$.getScript('/static/js/video-player.min.js', function() {
+				if ($('html').hasClass('ie9') || $('html').hasClass('ie8')) {
+					$.getScript('//player.ooyala.com/v3/9eba220ad98c47cda9fdf6ba82ce607a', function () {
+						init();
+					});
+				}
+				else {
+					$.getScript('//player.ooyala.com/v3/9eba220ad98c47cda9fdf6ba82ce607a?platform=html5', function () {
+						init();
+					});
+				}
+			});
 		}
 	}
 
