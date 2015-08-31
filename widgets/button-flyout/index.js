4e9368cc6a47904e390bbb325637bc80324ae8e7
Edward Chong <edward@ed-mart.com>
2015-08-31 13:06:55 -0700

    updates to button flyout.

--- widgets/button-flyout/index.js
+++ widgets/button-flyout/index.js
@@ -1,50 +1,49 @@
 $(function () {
-  if ($('.button-flyout').length) {
-    var buttonFlyout = $('.button-flyout');
+  if ($('#button-flyout').length) {
+    var buttonFlyout = $('#button-flyout');
 
 		buttonFlyout.find('> div').css({height: 'auto', width: 'auto'});
-		buttonFlyout.find('> div').data('width', buttonFlyout.find('> div').outerWidth(true));
-		buttonFlyout.find('> div').css('height', buttonFlyout.find('> div').outerHeight(true));
-		buttonFlyout.find('> div').css({width: 0});
+		buttonFlyout.data('width', buttonFlyout.find('> div').outerWidth(true));
+		//buttonFlyout.find('> div').css('height', buttonFlyout.find('> div').outerHeight(true));
+		buttonFlyout.css({'right': -1 * buttonFlyout.find('> div').outerWidth(true)});
 
-    $('.button-flyout').on('click', '> a', function (e) {
+    buttonFlyout.on('click', '> a', function (e) {
       e.preventDefault();
 
-      var bodyContent = $(this).parent().find('> div');
-
-      if (bodyContent.is(':visible')) {
-				bodyContent.css({width: 0});
+      if (parseInt($('#button-flyout').css('right')) != 0) {
+				$('#button-flyout').css('right', 0);
       }
 			else {
-				bodyContent.css({width: bodyContent.data('width')});
+				//bodyContent.css({width: bodyContent.data('width')});
+				$('#button-flyout').css('right', -1 * $('#button-flyout').data('width'));
       }
     });
 
-		$('.button-flyout').on('click', '.close', function () {
-			$(this).parent().parent().css({height: 0, width: 0});
+		$('#button-flyout').on('click', '.close', function () {
+			$('#button-flyout').css('right', -1 * $('#button-flyout').data('width'));
 		});
   }
 });
 
 function hideButtonFlyoutContent(w) {
   if (pageType > 1) {
-    $('.button-flyout').animate({
+    $('#button-flyout').animate({
       right: -w
     }, 500);
   }
 	else {
-    $('.button-flyout').animate({bottom: - $('.button-flyout').outerHeight()}, 500);
+    $('#button-flyout').animate({bottom: - $('#button-flyout').outerHeight()}, 500);
   }
 
-  $('.button-flyout').data('visible', false);
+  $('#button-flyout').data('visible', false);
 }
 
 function showButtonFlyoutContent() {
   if (pageType > 1) {
-    $('.button-flyout').animate({right: 0}, 500);
+    $('#button-flyout').animate({right: 0}, 500);
   }
 	else{
-    $('.button-flyout').animate({bottom: 0}, 500);
+    $('#button-flyout').animate({bottom: 0}, 500);
   }
-  $('.button-flyout').data('visible', true);
+  $('#button-flyout').data('visible', true);
 }
