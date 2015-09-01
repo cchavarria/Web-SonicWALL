dca1742b02d5b484ec7b83f2e73adfa193eabefe
Jorge A Leon <jorge@frontendify.com>
2015-09-01 14:31:07 -0700

    Added multipleSelect code to Search-pages folder under pages

--- pages/Search-pages/index.js
+++ pages/Search-pages/index.js
@@ -1,9 +1,45 @@
 /**
- * Created by jleon on 9/1/2015.
- */
-$('#content_type').multipleSelect({
-	placeholder: "testing This",
-	multiple: false,
-	selectAll: false,
-	single: true,
-})
+* Created by jleon on 9/1/2015.
+*/
+
+if ($.fn.multipleSelect) {
+	init();
+} else {
+	// load multiple select stylesheet
+	if ($('html').hasClass('ie8')) {
+		$('<link/>', {rel: 'stylesheet', href: '/static/library/css/multiple-select.css'}).appendTo('head');
+	}
+
+	//load multiple select plugin
+	$.getScript("/static/library/jQuery/jquery.multiple.select.js", function () {
+		$.fn.multipleSelect.defaults.onOpen = function (elem) {
+			var nextElem = $(elem).next(), ul = nextElem.find('ul');
+
+			if (ul.outerHeight() < ul.prop('scrollHeight') && !ul.data('width-fixed')) {
+				ul.css('width', ul.outerWidth() + $.position.scrollbarWidth());
+				ul.data('width-fixed', true);
+			}
+
+			//Check if dropdown needs to be reversed.
+			nextElem.css('right', 'auto');
+
+			if (nextElem.offset().left + nextElem.find('ul').outerWidth(true) > $('body').width()) {
+				nextElem.css('right', 0);
+			}
+			else {
+				nextElem.css('right', 'auto');
+			}
+		};
+
+		init();
+	});
+}
+
+function init() {
+	$('#content_type').multipleSelect({
+		placeholder: "testing This",
+		multiple: false,
+		selectAll: false,
+		single: true
+	})
+}
