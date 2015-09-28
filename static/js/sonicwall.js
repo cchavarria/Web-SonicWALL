029f13c7c7b80f0f21df47b0a780e6aa3d342458
elnaz82 <elnazd@gmail.com>
2015-09-28 14:41:35 -0700

    daily commit

--- static/js/sonicwall.js
+++ static/js/sonicwall.js
@@ -1,6 +1,19 @@
 $(function(){
-  $('nav').on('click', '.tier1 > li > a', function () {
-    $(this).parent().find('div.tier2 .col-sm-3').removeClass('active');
-    $(this).parent().find('div.tier2 .col-sm-3').first().addClass('active');
+  var nav = $('nav');
+  nav.on('click', '.tier1 > li > a', function () {
+    $('div.tier2 .container > div').removeClass('active');
+    $(this).parent().find('div.tier2 .container > div').first().addClass('active');
+  });
+
+  nav.on('click','div.tier2 .container > div > a', function(e){
+    if ($(this).attr('href') == '#') {
+      e.preventDefault();
+      $(this).parent().siblings().removeClass('active').end().addClass('active');
+    }
+  });
+
+  nav.on('click','.close',function(e){
+    e.preventDefault();
+    $(this).parents('li').removeClass('open');
   });
 });
