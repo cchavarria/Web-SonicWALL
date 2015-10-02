3163416fcd60738ce4f2da113eaa3bba60ffe2f3
Edward Chong <edward@ed-mart.com>
2015-10-02 12:09:24 -0700

    commit.

--- static/js/sonicwall.js
+++ static/js/sonicwall.js
@@ -1,26 +1,27 @@
 $(function () {
-	var nav = $('nav');
+	addResize('navigationFix', true);
 
-	nav.on('click', '.tier1 > li > a', function () {
-		addResize(function () {
-			if (pageType > 1) {
-				$('.tier1 > li').removeClass('open');//remove any open sub menu
+	$('#masthead').find('> nav')
+		.on('click', '.tier1 > li > a', function () {
+			navigationFix();
+
+			$('div.tier2 .container > div').removeClass('active');
+			$(this).parent().find('div.tier2 .container > div').first().addClass('active');
+		})
+		.on('click', 'div.tier2 .container > div > a', function (e) {
+			if ($(this).attr('href') == '#') {
+				e.preventDefault();
+				$(this).parent().siblings().removeClass('active').end().addClass('active');
 			}
-		}, true);
-
-		$('div.tier2 .container > div').removeClass('active');
-		$(this).parent().find('div.tier2 .container > div').first().addClass('active');
-	});
-
-	nav.on('click', 'div.tier2 .container > div > a', function (e) {
-		if ($(this).attr('href') == '#') {
+		})
+		.on('click', '.close', function (e) {
 			e.preventDefault();
-			$(this).parent().siblings().removeClass('active').end().addClass('active');
-		}
-	});
-
-	nav.on('click', '.close', function (e) {
-		e.preventDefault();
-		$(this).parents('li').removeClass('open');
-	});
+			$(this).parents('li').removeClass('open');
+		});
 });
+
+function navigationFix() {
+	if (pageType > 1) {
+		$('.tier1 > li').removeClass('open');//remove any open sub menu
+	}
+}
