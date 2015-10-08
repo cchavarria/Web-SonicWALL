3e302efe6e93c7a1b615c8c80b941e011fa9f0cc
elnaz82 <elnazd@gmail.com>
2015-10-08 10:28:06 -0700

    fixed border-b-gray fixed some alignment issues

--- static/js/off-canvas.js
+++ static/js/off-canvas.js
@@ -1,6 +1,6 @@
 //append off canvas content
 getLocalizedContent("LabelBack").done(function (data) {
-	$('.site-wrapper').after('<div id="off-canvas"><div class="bg-grey border-b-gray p-10"><a class="off-canvas-back"><i class="glyphicon glyphicon-menu-left"></i><span>' + data.LabelBack + '</span></a></div><div class="off-canvas-content"></div></div>');
+	$('.site-wrapper').after('<div id="off-canvas"><div class="bg-grey border-b-grey p-10"><a class="off-canvas-back"><i class="glyphicon glyphicon-menu-left"></i><span>' + data.LabelBack + '</span></a></div><div class="off-canvas-content"></div></div>');
 
 	$('#off-canvas').on('click', '.off-canvas-back', function (e) { //off canvas back button
 		e.preventDefault();
