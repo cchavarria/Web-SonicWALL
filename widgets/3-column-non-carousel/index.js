42f3036b80fd37c0be59dc0b39b84c2454229e20
elnaz82 <elnazd@gmail.com>
2015-10-29 18:23:37 -0700

    optimized widgets added plugin for match height columns added comments for each section in the widgets.

--- widgets/3-column-non-carousel/index.js
+++ widgets/3-column-non-carousel/index.js
@@ -1,37 +1,7 @@
 $(function () {
-  if ($('.set-height').length) {
-    addResize('heightFix', true);
-    if(pageType != 0){
-      setMaxHeight();
-    }
+  if($('.set-height').length){
+    $('.set-height').find('.row > div > div').matchHeight();
   }
 });
 
-function heightFix() {
-  if (pageType != 0) {
-    setMaxHeight();
-  } else {
-    $('.set-height').find(' > div > div > div').css('height', 'auto');
-  }
-}
 
-function setMaxHeight() {
-  $('.set-height').find('.row').each(function () {
-    var maxHeight = 0,
-        columns = $(this).find(' > div');
-    columns.each(function () {
-      maxHeight = $(this).height() > maxHeight ? $(this).height() : maxHeight;
-    });
-
-    if(pageType == 1){
-      columns.find('> div').each(function(){
-        if(!$(this).hasClass('height-auto-sm')){
-          $(this).css('height',maxHeight);
-        }
-      });
-    }else{
-      columns.find('> div').css('height',maxHeight);
-    }
-
-  });
-}
