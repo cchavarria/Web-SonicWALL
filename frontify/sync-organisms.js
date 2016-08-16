4be73e206793d436afa577b718d9301ac2e0f547
sjazemi <siamakmakhdoum@gmail.com>
2016-08-16 12:31:46 -0700

    Reorganised pattern based on Atomic design plug json files to sync patterns to Frontify
    Update:
    
    [Edward]: Modified frontify-responsive-header-footer.js to not grab localized content from DSG server.
    
    Change-Id: Ieffb59e3def13329ab0fbb0239d5b0cbe648aa4b

---
+++ frontify/sync-organisms.js
@@ -0,0 +1,10 @@
+var frontifyApi = require('@frontify/frontify-api');
+
+frontifyApi
+	.syncPatterns({
+		access_token: '0400bc2ed9a5ea9783ba6045ccbef6e37ccf4a19',
+		project: 94987
+	}, ['../widgets/organisms/**/*-frontify.json'])
+	.catch(function (err) {
+		console.error(err);
+	});
